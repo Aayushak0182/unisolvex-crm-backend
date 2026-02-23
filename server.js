5e1dfca3a55497f34958494c49446d8fc1b0a072
Aayush <aayushak0182@gmail.com>
2026-02-24 00:49:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "unisolvex123";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Incoming message:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
